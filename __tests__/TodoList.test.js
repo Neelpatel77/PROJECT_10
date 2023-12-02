8146731a2832825d79860c5d388c810146a32dff
oyebayofemi <oyebayooluwafemi@gmail.com>
2023-12-02 08:06:29 -0500

    implementing the TaskList Test Case
    Testing the TaskList Logic.

--- __tests__/TodoList.test.js
+++ __tests__/TodoList.test.js
@@ -1,9 +1,23 @@
-import { todoList, add } from "../TodoList";
+import { getTodoList, addTask, clearAll, changeStatus } from "../TodoList";
 
-describe("Todo list operations", ()=>{
-    it("Add function", ()=>{   
-        add("Do homework")
-        add("Walk the dog")     
-        expect(todoList.length).toBe(2)
+describe("Todo list operations", () => {
+    it("Add function", () => {
+        addTask("Do homework")
+        addTask("Walk the dog")
+        expect(getTodoList().length).toBe(2)
     })
+
+    it("Delete all tasks", () => {
+        addTask("Do homework")
+        addTask("Walk the dog")
+        clearAll()
+        expect(getTodoList().length).toBe(0)
+    })
+
+    it("ChangeStatus can be used to mark a task as complete", () => {
+        addTask("Do homework")
+        changeStatus(0)
+        expect(getTodoList()[0].isComplete).toBe(true)
+    })
+
 })
