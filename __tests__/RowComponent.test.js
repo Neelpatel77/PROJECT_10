4664a6ef12910bff0ad9bdcba4370dcd46621a81
Neel Patel <94647177+Neelpatel77@users.noreply.github.com>
2023-12-06 03:00:44 -0500

    Implementation of RowComponent.test.js

--- __tests__/RowComponent.test.js
+++ __tests__/RowComponent.test.js
@@ -1,13 +1,36 @@
 // Import the functions from the testing-library
-import {render, screen, fireEvent} from '@testing-library/react-native'
+import { render, screen, fireEvent } from '@testing-library/react-native';
 
 // Import the component to test
-import RowComponent from "../components/RowComponent"
+import RowComponent from "../components/RowComponent";
 
-describe("Row Component test", () => { 
-   it("Description of the test case should go here", () => {
-         // programatically generate the component
-         render(<RowComponent rowNumber={4}/>)
-         // do something with the component using the "screen" variable
-   })
-})
+const mockTodo = {
+  id: '1',
+  name: 'Test Todo',
+  isComplete: false
+};
+
+describe("RowComponent Tests", () => { 
+   it("renders the todo item correctly", () => {
+         // Programmatically generate the component with a mock todo item
+         render(<RowComponent todo={mockTodo} onToggle={() => {}} />);
+         
+         // Check if the component displays the todo item name
+         expect(screen.getByText('Test Todo')).toBeTruthy();
+   });
+
+   it("toggles the todo item status when the switch is clicked", () => {
+         // Mock the onToggle function
+         const onToggleMock = jest.fn();
+
+         // Render the RowComponent with the mock function
+         render(<RowComponent todo={mockTodo} onToggle={onToggleMock} />);
+
+         // Find the switch and fire the value change event
+         const toggle = screen.getByRole('switch');
+         fireEvent(toggle, 'onValueChange', true);
+
+         // Expect the onToggle function to have been called with the correct id
+         expect(onToggleMock).toHaveBeenCalledWith(mockTodo.id);
+   });
+});
